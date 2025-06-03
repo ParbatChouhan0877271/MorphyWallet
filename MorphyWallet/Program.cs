aa8b8949c0bd44797d2a4121a88e9c1048204eae
Parbat <w0877271@myscc.ca>
2025-06-03 12:37:58 -0400

    Changes in Wallet , home controller and catalogue,aboutus and layout updated

--- MorphyWallet/Program.cs
+++ MorphyWallet/Program.cs
@@ -12,10 +12,10 @@ namespace MorphyWallet
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllersWithViews();
-
+           
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+            //add this applicationuser with builder so it will configure 
             builder.Services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
